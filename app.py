71c60a3df5024622fdf7c2c0a3f62711c653e74a
William Sebastian Minchola Robles <ws.mincholar@alum.up.edu.pe>
2026-06-29 13:14:12 -0500

    Update caption in Streamlit app

--- app.py
+++ app.py
@@ -277,7 +277,7 @@ Respuesta:
 # Interfaz Streamlit
 # =============================================================
 st.title("📄 Plataforma Cloud-Native de Búsqueda Semántica en PDFs")
-st.caption("Streamlit + Docker + Azure Web App for Container + MongoDB Atlas + Cohere + Gemini")
+st.caption("Streamlit + Docker + Azure Web App for Container + MongoDB Atlas + Cohere + Gemini + DEPLOY YAMAL")
 
 with st.sidebar:
     st.header("Configuración")
